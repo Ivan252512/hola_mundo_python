f33f4ce4cc2d220d2466aa92f1abd1a03281bb3a
Carlos Iván Pineda Santiago <ivan31416neda@gmail.com>
2023-04-27 23:11:17 -0600

    first commit

---
+++ hola_mundo.py
@@ -0,0 +1,35 @@
+hola_mundo = "Hola  Mundo"
+
+print(type(hola_mundo))
+
+print(hola_mundo)
+
+
+hola_mundo = 55
+
+print(type(hola_mundo))
+
+print(hola_mundo)
+
+
+hola_mundo = b'011'
+
+print(type(hola_mundo))
+
+print(hola_mundo)
+
+
+# for i in range(100):
+#    print(f"Hola alumno número: {i}")
+
+count = 0
+
+#while count < 100:
+#    if count % 2 == 0:
+#        print("Hola par: ", count)
+#    else:
+#        print("Hola impar: ", count)
+#    count += 1
+
+for i in range(100):
+    print("*" *  i)
